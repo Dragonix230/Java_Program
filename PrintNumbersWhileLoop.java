e5330f48fa0c9daf06cd88b115b04001333e352f
Ishan Joshi <167687329+Dragonix230@users.noreply.github.com>
2025-01-03 17:26:53 +0530

    Create PrintNumbersWhileLoop.java

---
+++ PrintNumbersWhileLoop.java
@@ -0,0 +1,10 @@
+public class PrintNumbersWhileLoop {
+    public static void main(String[] args) {
+        int number = 0;
+
+        while (number <= 5) {
+            System.out.println(number);
+            number++;
+        }
+    }
+}
