7b7c01a9d3a2fb1052e61cc4bca4240ecc08e896
Ishan Joshi <167687329+Dragonix230@users.noreply.github.com>
2025-01-06 23:46:39 +0530

    Create MultiplicationTables.java

---
+++ MultiplicationTables.java
@@ -0,0 +1,9 @@
+public class MultiplicationTables {
+    public static void main(String[] args) {
+        int number = 2; // For table of 2
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+}
